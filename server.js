f2ffdcb5993534e0f88a740f37a0939bdf58e1aa
shuvosvc <5arafatshovo@gmail.com>
2022-08-30 21:53:52 +0600

    feat: vercel config

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const cors = require("cors");
 const Socket = require("socket.io");
+const logger = require("morgan");
 const { getCard } = require("./DB/Cards");
 const {
   createServer,
@@ -12,6 +13,7 @@ const {
 const { addPlayer, removePlayer, getPlayer } = require("./DB/PlayersSockets");
 
 const app = express();
+app.use(logger("dev"));
 app.use(cors());
 const server = http.createServer(app);
 const io = Socket(server, {
